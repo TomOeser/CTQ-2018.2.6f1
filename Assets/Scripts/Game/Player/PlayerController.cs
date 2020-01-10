670644c7c8b7bef8f7413eee372cb199d13caf37
Tom Oeser <tom.oeser@googlemail.com>
2020-01-10 18:11:34 +0100

    PrePushing Motor

--- Assets/Scripts/Game/Player/PlayerController.cs
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -37,6 +37,7 @@ public class PlayerController : Bolt.EntityEventListener<IPlayerState>
             state.health = (byte)Mathf.Clamp(state.health + 1, 0, 100);
         }
 
+        /*
         var speed = 5f;
         var movement = Vector3.zero;
         if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
@@ -45,7 +46,7 @@ public class PlayerController : Bolt.EntityEventListener<IPlayerState>
         if (movement != Vector3.zero)
         {
             transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
-        }
+        }*/
     }
 
     public override void SimulateController()
@@ -70,10 +71,67 @@ public class PlayerController : Bolt.EntityEventListener<IPlayerState>
         input.Token = new TestToken();
 
         entity.QueueInput(input);*/
+
+        IPlayerCommandInput input = PlayerCommand.Create();
+
+        input.
+
+        var speed = 5f;
+        var movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+
+
+
+
     }
     public override void ExecuteCommand(Bolt.Command c, bool resetState)
     {
+        if (state.dead) return;
 
+        PlayerCommand cmd = (PlayerCommand)c;
+
+        if (resetState)
+			{
+				_motor.SetState(cmd.Result.position, cmd.Result.velocity, cmd.Result.isGrounded, cmd.Result.jumpFrames);
+			}
+			else
+			{
+				// move and save the resulting state
+				var result = _motor.Move(cmd.Input.forward, cmd.Input.backward, cmd.Input.left, cmd.Input.right, cmd.Input.jump, cmd.Input.yaw);
+
+				cmd.Result.position = result.position;
+				cmd.Result.velocity = result.velocity;
+				cmd.Result.jumpFrames = result.jumpFrames;
+				cmd.Result.isGrounded = result.isGrounded;
+
+				if (cmd.IsFirstExecution)
+				{
+					// animation
+					AnimatePlayer(cmd);
+
+					// set state pitch
+					state.pitch = cmd.Input.pitch;
+					state.weapon = cmd.Input.weapon;
+					state.Aiming = cmd.Input.aiming;
+
+					// deal with weapons
+					if (cmd.Input.aiming && cmd.Input.fire)
+					{
+						FireWeapon(cmd);
+					}
+				}
+
+				if (entity.IsOwner)
+				{
+					cmd.Result.Token = new TestToken();
+				}
+			}
     }
 
 
